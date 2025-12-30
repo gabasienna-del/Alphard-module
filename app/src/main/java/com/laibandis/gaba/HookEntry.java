9c3f0dd035ff80b9e673d7d4daafc3d1d847e643
gabasienna-del <gabasienna-del@users.noreply.github.com>
2025-12-31 03:56:33 +0500

    WS order interceptor

--- app/src/main/java/com/laibandis/gaba/HookEntry.java
+++ app/src/main/java/com/laibandis/gaba/HookEntry.java
@@ -13,3 +13,18 @@ public class HookEntry implements IXposedHookLoadPackage {
         android.util.Log.e("ALPHARD", "LSPosed MODULE LOADED");
     }
 }
+
+        try {
+            Class<?> ws = Class.forName("okhttp3.internal.ws.RealWebSocket", false, lpparam.classLoader);
+
+            de.robv.android.xposed.XposedHelpers.findAndHookMethod(ws, "onReadMessage",
+                    String.class,
+                    new de.robv.android.xposed.XC_MethodHook() {
+                        protected void afterHookedMethod(MethodHookParam param) {
+                            String msg = (String) param.args[0];
+                            if (msg.contains("order") || msg.contains("queue") || msg.contains("trip")) {
+                                android.util.Log.e("ALPHARD-WS", msg);
+                            }
+                        }
+                    });
+        } catch (Throwable t) {}
