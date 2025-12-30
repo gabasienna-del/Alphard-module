b490dbaca7dce0a30b69c6a1c397897c08dd3dd5
Gabit <_@.com>
2025-12-30 20:03:56 +0500

    Add Alphard inDriver policy core

---
+++ app/src/main/java/com/laibandis/gaba/policy/HookEntry.java
@@ -0,0 +1,58 @@
+package com.laibandis.gaba.policy;
+
+import android.app.AlarmManager;
+import android.app.Application;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.os.Build;
+import android.os.PowerManager;
+import android.net.ConnectivityManager;
+
+import de.robv.android.xposed.*;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class HookEntry implements IXposedHookLoadPackage {
+
+    static final String TARGET = "sinet.startup.inDriver";
+
+    @Override
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+
+        if (!lpparam.packageName.equals(TARGET)) return;
+
+        XposedBridge.log("🔥 Alphard policy loaded for inDriver");
+
+        XposedHelpers.findAndHookMethod(Application.class, "onCreate", new XC_MethodHook() {
+            @Override protected void afterHookedMethod(MethodHookParam param) {
+
+                Context ctx = (Context) XposedHelpers.callMethod(param.thisObject, "getApplicationContext");
+                PowerManager pm = (PowerManager) ctx.getSystemService(Context.POWER_SERVICE);
+
+                try {
+                    PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "Alphard:inDriver");
+                    wl.acquire();
+                } catch (Throwable t) {}
+
+                try {
+                    pm.addThermalStatusListener(status -> {});
+                } catch (Throwable t) {}
+            }
+        });
+
+        XposedHelpers.findAndHookMethod(AlarmManager.class, "set",
+                int.class, long.class, PendingIntent.class,
+                new XC_MethodHook() {
+                    @Override protected void beforeHookedMethod(MethodHookParam param) {
+                        param.args[0] = AlarmManager.RTC_WAKEUP;
+                    }
+                });
+
+        XposedHelpers.findAndHookMethod(AlarmManager.class, "setExact",
+                int.class, long.class, PendingIntent.class,
+                new XC_MethodHook() {
+                    @Override protected void beforeHookedMethod(MethodHookParam param) {
+                        param.args[0] = AlarmManager.RTC_WAKEUP;
+                    }
+                });
+    }
+}
