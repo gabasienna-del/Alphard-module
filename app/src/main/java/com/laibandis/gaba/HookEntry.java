9185570125173aad8a392bb981e9c7d5a7951812
gabasienna-del <gabasienna-del@users.noreply.github.com>
2025-12-31 00:41:08 +0500

    Convert project into real LSPosed module

--- app/src/main/java/com/laibandis/gaba/HookEntry.java
+++ app/src/main/java/com/laibandis/gaba/HookEntry.java
@@ -1,35 +1,12 @@
 package com.laibandis.gaba;
 
-import de.robv.android.xposed.*;
+import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class HookEntry implements IXposedHookLoadPackage {
-
     @Override
-    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         if (!lpparam.packageName.equals("sinet.startup.inDriver")) return;
-
-        XposedBridge.log("🚗 Indriver Hook loaded");
-
-        try {
-            XposedHelpers.findAndHookMethod(
-                    "okhttp3.RealWebSocket",
-                    lpparam.classLoader,
-                    "onReadMessage",
-                    String.class,
-                    new XC_MethodHook() {
-                        @Override
-                        protected void afterHookedMethod(MethodHookParam param) {
-                            String msg = (String) param.args[0];
-                            if (msg != null && msg.contains("ORDER")) {
-                                XposedBridge.log("📦 ORDER FRAME: " + msg);
-                            }
-                        }
-                    }
-            );
-        } catch (Throwable t) {
-            XposedBridge.log("Hook error: " + t);
-        }
+        // hooks here
     }
 }
