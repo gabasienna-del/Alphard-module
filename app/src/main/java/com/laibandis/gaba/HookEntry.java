076e0b73c9cfab314ec3aa1302f54f9d26db404c
gabasienna-del <gabasienna-del@users.noreply.github.com>
2025-12-31 03:49:26 +0500

    Remove okio dependency

--- app/src/main/java/com/laibandis/gaba/HookEntry.java
+++ app/src/main/java/com/laibandis/gaba/HookEntry.java
@@ -1,11 +1,7 @@
 package com.laibandis.gaba;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedBridge;
-import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
-import okio.ByteString;
 
 public class HookEntry implements IXposedHookLoadPackage {
 
@@ -14,48 +10,6 @@ public class HookEntry implements IXposedHookLoadPackage {
 
         if (!lpparam.packageName.equals("sinet.startup.inDriver")) return;
 
-        XposedBridge.log("🔥 LATENCY-PROBE injected");
-
-        // TEXT FRAMES
-        XposedHelpers.findAndHookMethod(
-                "okhttp3.internal.ws.RealWebSocket",
-                lpparam.classLoader,
-                "onReadMessage",
-                String.class,
-                new XC_MethodHook() {
-                    protected void beforeHookedMethod(MethodHookParam param) {
-                        String msg = (String) param.args[0];
-                        XposedBridge.log("🟢 WS TEXT => " + msg);
-                    }
-                }
-        );
-
-        // BINARY FRAMES
-        XposedHelpers.findAndHookMethod(
-                "okhttp3.internal.ws.RealWebSocket",
-                lpparam.classLoader,
-                "onReadMessage",
-                ByteString.class,
-                new XC_MethodHook() {
-                    protected void beforeHookedMethod(MethodHookParam param) {
-                        ByteString bs = (ByteString) param.args[0];
-                        XposedBridge.log("🟡 WS BIN => " + bs.hex());
-                    }
-                }
-        );
-
-        // FIREBASE PUSH (FCM)
-        XposedHelpers.findAndHookMethod(
-                "com.google.firebase.messaging.FirebaseMessagingService",
-                lpparam.classLoader,
-                "onMessageReceived",
-                "com.google.firebase.messaging.RemoteMessage",
-                new XC_MethodHook() {
-                    protected void beforeHookedMethod(MethodHookParam param) {
-                        Object rm = param.args[0];
-                        XposedBridge.log("🔔 FCM PUSH => " + rm.toString());
-                    }
-                }
-        );
+        android.util.Log.e("ALPHARD", "LSPosed MODULE LOADED");
     }
 }
