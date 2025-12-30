7e091b2d4841ac9122571d5cafd3b4418fdb87e7
gabasienna-del <gabasienna-del@users.noreply.github.com>
2025-12-31 00:26:52 +0500

    Clean project with Gradle 8.7

---
+++ app/src/main/java/com/laibandis/gaba/MainActivity.java
@@ -0,0 +1,6 @@
+package com.laibandis.gaba;
+import android.app.Activity;
+import android.os.Bundle;
+public class MainActivity extends Activity {
+  @Override protected void onCreate(Bundle b) { super.onCreate(b); }
+}
